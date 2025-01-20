1405805dd545b5275df20fc1c17f2f4b849eb416
Diya Panchal <diya.pce21@sot.pdpu.ac.in>
2025-01-20 17:00:18 +0530

    basic get route api

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from "express";
+import apiRouter from "./routes/api.js";
 
 const app = express();
 const PORT = process.env.PORT || 3002;
+
+app.use("/", apiRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is listening at http://localhost:${PORT}`);
